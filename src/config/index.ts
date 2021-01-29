9556e4124cd9514565225ac040747c31bcfa5529
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2021-01-29 18:31:15 +0100

    feat(config/index.ts): defaultBrowserConfiguration created

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,10 @@ const defaultConfig = {
   urls: {
     authentication: 'https://www.facebook.com/',
   },
+  defaultBrowserConfiguration: {
+    headless: true,
+    slowMo: 50,
+  },
 };
 
 export { defaultConfig };
